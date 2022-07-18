0a13f852122e604af3c53ff3a2eecb8b7e511769
mateo1761 <mateoquinterozapata@gmail.com>
2022-07-18 17:17:18 -0500

    Se crea vista de editar y eliminar usuario

--- resources/views/empleado/edit.blade.php
+++ resources/views/empleado/edit.blade.php
@@ -1,1 +1,7 @@
-Formulario de edicion  de empleado
+<h1>Formulario de edicion  de empleado</h1>
+
+<form action="{{ url('/empleado/'.$empleado->id) }}" method="post">@csrf
+    {{ method_field('PATCH') }}
+    
+    @include('empleado.form')
+</form>
