9e16ba6468767f3be9c3cca84244ef90bf6b91ad
mateo1761 <mateoquinterozapata@gmail.com>
2022-07-18 17:16:28 -0500

    Se crea vista de creacion de usuario

--- resources/views/empleado/create.blade.php
+++ resources/views/empleado/create.blade.php
@@ -1,1 +1,7 @@
-Formulario de creacion de empleado
+<h1>Formulario de creacion de empleado</h1>
+
+<form action="{{ url('/empleado') }}" method="post" enctype="multipart/form-data">
+@csrf
+@include('empleado.form')
+
+</form>
