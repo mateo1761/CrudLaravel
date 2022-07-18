6991aba8222ba0c4216c99cbc3fc3d56a946f259
mateo1761 <mateoquinterozapata@gmail.com>
2022-07-18 17:19:27 -0500

    Se crea template de formulario

--- resources/views/empleado/form.blade.php
+++ resources/views/empleado/form.blade.php
@@ -1,1 +1,17 @@
-Formulario que tendra datos en comun con create y edit
+<label for="Nombre"> Nombre</label>
+<input type="text" name="Nombre" value="{{ $empleado->Nombre }}" id="Nombre">
+<br/>
+
+<label for="Apellido"> Apellido</label>
+<input type="text" name="Apellido" value="{{ $empleado->Apellido }}" id="Apellido">
+<br/>
+
+<label for="Cedula"> Cedula</label>
+<input type="text" name="Cedula" value="{{ $empleado->Cedula }}" id="Cedula" maxlength="11">
+<br/>
+
+<label for="Correo"> Correo</label>
+<input type="text" name="Correo" value="{{ $empleado->Correo }}" id="Correo">
+<br/>
+
+<input type="submit" name="Enviar" id="Enviar" value="Guardar">
