fd2a3206550c3265201e843ff79292f7fd585376
mateo1761 <mateoquinterozapata@gmail.com>
2022-07-18 17:19:57 -0500

    Se crea pagina principal

--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -1,1 +1,36 @@
-Mostrar la lista de empleados
+<table class="table table-light">
+    <thead class="thead-light">
+        <tr>
+            <th>id</th>
+            <th>Cedula</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Correo</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        @foreach( $empleados as $empleado )
+        <tr>
+            <td>{{ $empleado->id }}</td>
+            <td>{{ $empleado->Cedula }}</td>
+            <td>{{ $empleado->Nombre }}</td>
+            <td>{{ $empleado->Apellido }}</td>
+            <td>{{ $empleado->Correo }}</td>
+            <td>
+            
+            <a href="{{ url('/empleado/'.$empleado->id.'/edit') }}">Editar</a>
+
+            <form action="{{ url('/empleado/'.$empleado->id) }}" method="post">
+            @csrf
+            {{ method_field('DELETE') }}
+            <input type="submit" onclick="return confirm('¿Realmente quieres eliminar este registro?')" value="Borrar"></form>
+
+
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+
+</table>
